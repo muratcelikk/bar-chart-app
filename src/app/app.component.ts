58bf7231b2c2755ded22adffc5014596438b681e
Murat Çelik <muratcelik11@gmail.com>
2021-08-30 17:48:53 +0300

    via

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ChartOptions, ChartType, ChartDataSets } from 'chart.js';
+import { Label } from 'ng2-charts';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'bar-chart-app';
+  public barChartOptions: ChartOptions = {
+    responsive: true,
+  };
+  public barChartLabels: Label[] = ['2015', '2016', '2017', '2018', '2019', '2020'];
+  public barChartType: ChartType = 'bar';
+  public barChartLegend = true;
+  public barChartPlugins = [];
+  
+  public barChartData: ChartDataSets[] = [
+    { data: [65, 67, 70, 75, 80, 90], label: 'Js' },
+    { data: [50, 48, 47, 49, 44, 40], label: '.Net' },
+    { data: [40, 30, 28, 25, 22, 20], label: 'Java' },
+  ];
+  
+  constructor() { }
+  
+  ngOnInit() {
+  }
 }
